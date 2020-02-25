182d909d12297822688a30e7011adbee2091d95f
MaciejSpalek <spwrtt@gmail.com>
2020-02-25 10:41:49 +0100

    feat: add navbar, add variables.scss, style login form and install fontawesome package

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import Login from './Containers/Login/login';
 import Home from './Containers/Home/home';
-import app from './Components/base'
+import app from './Components/base';
+import Navbar from './Components/navbar/navbar';
 import './App.scss';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      user: {}
+      user: null
     }
   }
   
@@ -29,6 +30,7 @@ class App extends Component {
   render() {
     return (
     <div className="App">
+      <Navbar/>
       {this.state.user ? (<Home/>): (<Login/>)}
     </div>
     );
--- src/Containers/Login/login.js
+++ src/Containers/Login/login.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import app from '../../Components/base'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUserPlus } from '@fortawesome/free-solid-svg-icons'
 
 class Login extends Component {
     constructor(props) {
@@ -44,9 +46,9 @@ class Login extends Component {
 
     render() {
         return (
-            <section className="logPanel">
+            <section className="login">
                 <form className="form">
-                    <div className="form__input-box">
+                    {/* <div className="form__input-box"> */}
                         <input 
                             className="form__email form__input" 
                             type="email" 
@@ -55,7 +57,6 @@ class Login extends Component {
                             onChange={this.handleChange} 
                             value={this.state.email}
                         />
-
                         <input 
                             className="form__password form__input" 
                             type="password" 
@@ -64,10 +65,13 @@ class Login extends Component {
                             onChange={this.handleChange} 
                             value={this.state.password}
                         />
-                    </div>
-                    <div className="form__button-box">
+                    {/* </div> */}
+                    {/* <div className="form__button-box"> */}
                         <button className="form__button form__button--login" onClick={this.logIn}>Log in</button>
-                        <button className="form__button form__button--singUp" onClick={this.signUp}>Sign up</button>
+                        {/* <button className="form__button form__button--singUp" onClick={this.signUp}>Sign up</button> */}
+                    {/* </div> */}
+                    <div className="form__logo">
+                        <FontAwesomeIcon icon={faUserPlus} color="#005D95" style={{fontSize:60}}/>
                     </div>
                 </form>
             </section>
