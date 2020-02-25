55a6eb81ad96d5e470bb75e743ecfe990a101542
MaciejSpalek <spwrtt@gmail.com>
2020-02-25 08:29:36 +0100

    refactor: change app structur and add authentication

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
-import React from 'react';
-import LogPanel from './Containers/logPanel/logPanel';
+import React, { Component } from 'react';
+import Login from './Containers/Login/login';
+import Home from './Containers/Home/home';
+import app from './Components/base'
 import './App.scss';
 
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {}
+    }
+  }
+  
+  componentDidMount() {
+    this.authListener();
+  }
 
-function App() {
-  return (
+  authListener() {
+    app.auth().onAuthStateChanged((user) => {
+      if(user) {
+        this.setState({user})
+      } else {
+        this.setState({user: null})
+      }
+    });
+  }
+
+  render() {
+    return (
     <div className="App">
-      <LogPanel/>
+      {this.state.user ? (<Home/>): (<Login/>)}
     </div>
-  );
+    );
+  }
 }
 
 export default App;
---
+++ src/Containers/Login/login.js
@@ -0,0 +1,78 @@
+import React, { Component } from "react";
+import app from '../../Components/base'
+
+class Login extends Component {
+    constructor(props) {
+        super(props)
+        this.logIn = this.logIn.bind(this);
+        this.signUp = this.signUp.bind(this);
+        this.handleChange = this.handleChange.bind(this)
+        this.state = {
+            email: "",
+            password: ""
+        }
+    }
+
+    logIn(e) {
+        e.preventDefault();
+        app.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
+        .then(user => {
+            console.log(user)
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+        
+    }
+
+    signUp(e) {
+        e.preventDefault();
+        app.auth().createUserWithEmailAndPassword(this.state.email, this.state.password)
+        .then(user => {
+            console.log(user)
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+    }
+ 
+    handleChange(e) {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    render() {
+        return (
+            <section className="logPanel">
+                <form className="form">
+                    <div className="form__input-box">
+                        <input 
+                            className="form__email form__input" 
+                            type="email" 
+                            name="email"
+                            placeholder="email" 
+                            onChange={this.handleChange} 
+                            value={this.state.email}
+                        />
+
+                        <input 
+                            className="form__password form__input" 
+                            type="password" 
+                            name="password"
+                            placeholder="password" 
+                            onChange={this.handleChange} 
+                            value={this.state.password}
+                        />
+                    </div>
+                    <div className="form__button-box">
+                        <button className="form__button form__button--login" onClick={this.logIn}>Log in</button>
+                        <button className="form__button form__button--singUp" onClick={this.signUp}>Sign up</button>
+                    </div>
+                </form>
+            </section>
+        );
+    }
+}
+
+export default Login
