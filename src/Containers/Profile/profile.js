9afea0dc08e5c4e357fe4f23f97eacbc4475cabe
MaciejSpalek <spwrtt@gmail.com>
2020-03-12 10:40:42 +0100

    wip: style user profile, add rotating card

--- src/Containers/Profile/profile.js
+++ src/Containers/Profile/profile.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 import app from "../../Components/base";
 import styled from "styled-components"
+import classNames from "classnames";
 import * as styleHelpers  from '../../Components/styleHelpers'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUserSecret, faImages, faPenSquare, faCameraRetro } from '@fortawesome/free-solid-svg-icons'
+import { faUserSecret, faImages, faPenSquare, faCameraRetro, faUser, faExternalLinkSquareAlt } from '@fortawesome/free-solid-svg-icons'
 
 const flexCenter = styleHelpers.flexCenter;
 const variables = styleHelpers.variables;
@@ -19,15 +20,40 @@ const Container = styled.section`
     padding: .5em;
     overflow-y: scroll;
 `
-const Wrapper = styled.div`
-    ${flexCenter}
-    justify-content: flex-start;
-    flex-direction: column;
+const ProfileCard = styled.div`
+    ${flexCenter};
+    position: relative;
     width: 280px;
     height: 500px;
-    /* border: .2em solid ${variables.$blue}; */
-    box-shadow: 0 .5em .5em .1em black;
+`
+
+
+
+const Frontside = styled.div`
+    ${flexCenter}
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
     border-radius: .5em;
+    transition: .3s linear;
+    transform-origin: center;
+    z-index: 1;
+    backface-visibility: hidden;
+    box-shadow: 0 .2em .5em .1em black;
+`
+const Backside = styled.div`
+    ${flexCenter};
+    justify-content: space-between;
+    flex-direction: column;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    border-radius: .5em;
+    transition: .3s linear;
+    transform-origin: center;
+    transform:  rotateY(180deg);
+    z-index: -1;
+    box-shadow: 0 .5em .5em .1em black;
 `
 const PhotoBox = styled.section`
     ${flexCenter}
@@ -37,20 +63,8 @@ const PhotoBox = styled.section`
     background-color: ${variables.$blue};
     border-top-right-radius: .5em;
     border-top-left-radius: .5em;
+    transition: .5s linear;
 `
-const InfoBox = styled.section`
-    ${flexCenter}
-    justify-content: flex-start;
-    flex-direction: column;
-    position: relative;
-    width: 100%;
-    flex:1;
-    background-color: ${variables.$darkBlue };
-    border-bottom-right-radius: .5em;
-    border-bottom-left-radius: .5em;
-    padding: 0 .5em;
-`
-
 const Nick = styled.span`
     position:absolute;
     bottom: .5em;
@@ -60,59 +74,89 @@ const Nick = styled.span`
     color: white;
     line-height:1;
 `
-
 const ButtonBox = styled.div`
     ${flexCenter}
     justify-content: flex-end;
     width: 100%;
-    border-bottom: .2em solid ${variables.$blue};
-    padding: .1em;
+    background-color: ${variables.$darkBlue};
+    border-radius: .5em;
+`
+
+
+
+const AddBox = styled.div`
+    display:grid;
+    grid-template-columns: repeat(1, 1fr);
+    grid-gap: .5em;
+    width: 100%;
+    border-bottom-right-radius: .5em;
+    border-bottom-left-radius: .5em;
+    padding: .5em;
 `
 const Input = styled.input`
     grid-gap: 0;
-    width: 80%;
-    border-radius: .2em;
+    width: 100%;
     height: 35px;
     font-size: 1em;
     font-weight: bold;
     border-radius: 0.2em;
+    background-color: white;
     border: none;
-    margin: 0 auto;
     padding: 0 .3em;
+    color: ${variables.$grayBlue};
     &::placeholder {
         color: ${variables.$blue};
         font-weight: 100;
     }
 
 `
-const AddBox = styled.div`
-    display:grid;
-    grid-template-columns: repeat(2, 1fr);
-    grid-template-rows: repeat(4, 1fr);
-    width: 100%;
-    height: 100%;
-    padding: .5em;
-`
-
 
 
 class Profile extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isEditButtonActive: false
+        }
+    }
+
     capitalizeFirstLetter(string) {
         return string.charAt(0).toUpperCase() + string.slice(1);
     }
+
+    editButtonHandle() {
+        this.setState(prevState => ({
+            isEditButtonActive: !prevState.isEditButtonActive
+        }))
+    }
     render() {
+        const { isEditButtonActive } = this.state;
+        const frontActive = {
+            transform:  "rotateY(360deg)",
+            zIndex: 1
+        }
+        const backActive = {
+            transform: "rotateY(180deg)"
+        }
+       
         return (
             <Container>
-                <Wrapper>
-                    <PhotoBox>
-                        <FontAwesomeIcon icon={faUserSecret} style={{fontSize: 150}} color={variables.$darkBlue} />
-                        <Nick> {app.getCurrentUser() ? this.capitalizeFirstLetter(app.getCurrentUser().displayName) : null} </Nick>
-                    </PhotoBox>
-                    <InfoBox>
+                <ProfileCard>
+                    <Frontside style={isEditButtonActive ? backActive : null}>
+                        <PhotoBox>
+                            <FontAwesomeIcon icon={faUser} style={{fontSize: 150}} color={variables.$darkBlue} />
+                            <Nick> {app.getCurrentUser() ? this.capitalizeFirstLetter(app.getCurrentUser().displayName) : null} </Nick>
+                        </PhotoBox>
                         <ButtonBox>
-                            <FontAwesomeIcon icon={faCameraRetro} style={{fontSize: 40, margin: '.1em'}} color={variables.$orange} />
-                            <FontAwesomeIcon icon={faImages} style={{fontSize: 40, margin: '.1em'}} color={variables.$orange}/>
-                            <FontAwesomeIcon icon={faPenSquare} style={{fontSize: 40, margin: '.1em'}} color={variables.$orange} />
+                            <FontAwesomeIcon icon={faCameraRetro} style={{fontSize: 35, margin: '.1em'}} color={variables.$orange} />
+                            <FontAwesomeIcon icon={faImages} style={{fontSize: 35, margin: '.1em'}} color={variables.$orange}/>
+                            <FontAwesomeIcon icon={faExternalLinkSquareAlt} style={{fontSize: 35, margin: '.1em'}} color={variables.$orange}  onClick={this.editButtonHandle.bind(this)}/>
+                        </ButtonBox>
+                    </Frontside>
+                    <Backside style={isEditButtonActive ? frontActive : null}>
+                        <ButtonBox style={{borderBottomLeftRadius: 0, borderBottomRightRadius: 0}}>
+                            <FontAwesomeIcon icon={faPenSquare} style={{fontSize: 35, margin: '.1em'}} color={variables.$orange}/>
+                            <FontAwesomeIcon icon={faExternalLinkSquareAlt} style={{fontSize: 35 , margin: '.1em'}} color={variables.$orange}  onClick={this.editButtonHandle.bind(this)}/>
                         </ButtonBox>
                         <AddBox>
                             <Input placeholder="staż tren."></Input>
@@ -120,9 +164,13 @@ class Profile extends Component {
                             <Input placeholder="waga"></Input>
                             <Input placeholder="wzrost"></Input>
                             <Input placeholder="sport"></Input>
-                        </AddBox>
-                    </InfoBox>
-                </Wrapper>
+                            <Input placeholder="płeć"></Input>
+
+                            {/* <Input placeholder="..."></Input>
+                            <Input placeholder="..."></Input> */}
+                        </AddBox> 
+                    </Backside>
+                </ProfileCard>
             </Container>
         )
     }
